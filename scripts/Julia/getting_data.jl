6385348f63d00a0ef88115d484517659a810e477
newton-c <cnewton2@buffalo.edu>
2021-10-27 13:50:32 -0400

    backing up progress

--- scripts/Julia/getting_data.jl
+++ scripts/Julia/getting_data.jl
@@ -1,7 +1,12 @@
 using WorldBankData
+using DataFrames
+
+wb_countries = wdi(["VC.IHR.PSRC.P5"], "all", 1990, 2018);
+wb_countries! = dropmissing(wb_countries, :VC_IHR_PSRC_P5);
+wb_countries! = unique(wb_countries[!, :iso2c]);
 
 wb_data = wdi(["SI.POV.GINI", "VC.IHR.PSRC.P5", "SL.UEM.TOTL.NE.ZS",
-              "SE.XPD.TOTL.GD.ZS"], "all", 1989, 2018)
+              "SE.XPD.TOTL.GD.ZS"], wb_countries, 1989, 2018)
 
 rename!(wb_data, Dict(:SI_POV_GINI => :gini, :VC_IHR_PSRC_P5 => :hom_rate,
                       :SL_UEM_TOTL_NE_ZS => :unemployment,
