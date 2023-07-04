ddaa8b0722d585000dc28a6aeb9eeac68b96e6b4
newton-c <newton.christop@gmail.com>
2023-07-04 08:57:15 -0300

    adding single dataset viz

--- scripts/models.R
+++ scripts/models.R
@@ -2,17 +2,20 @@ library(rstanarm)
 library(tidyverse)
 library(bayesplot)
 
-m1 <- stan_glm.nb(hom_count ~ Csdum + resources + duration + battle_deaths,
-                  data = subset(a.out$imputations[[1]], postcon == 1))
+set.seed(977634)
 
+#m1 <- stan_glm.nb(hom_count ~ Csdum + resources + duration + battle_deaths,
+#                  data = subset(a.out$imputations[[1]], postcon == 1))
 
-m2 <- stan_glm.nb(hom_count ~ Csdum + resources + duration + battle_deaths +
-                  pop_slums + education,
-              data = subset(a.out$imputations[[1]], postcon == 1))
+#
+#m2 <- stan_glm.nb(hom_count ~ Csdum + resources + duration + battle_deaths +
+#                  pop_slums + education,
+#              data = subset(a.out$imputations[[1]], postcon == 1))
 
 library(gridExtra)
 library(patchwork)
 
+theme_set(theme_bw())
 # define models
 dum_models <- function(i) {
     fit <- stan_glm.nb(hom_count ~ Csdum + resources + duration +
@@ -95,7 +98,7 @@ layout_grid <- rbind(c(2, 3, 4),
 nb_grid <- arrangeGrob(grobs = dum_plots, nrow = 3, ncol = 3,
                        layout_matrix = layout_grid)
 
-ggsave("figures/dum_coef_plot.png", nb_grid, height = 10, width = 7)
+ggsave("figures/dum_coef_plot07-04.png", nb_grid, height = 10, width = 7)
 
 # child soldier index
 coef_names <- c("Intercept", "Child Soldier\n(Index)", "Illicit Resources",
@@ -124,7 +127,7 @@ layout_grid <- rbind(c(2, 3, 4),
 nb_grid <- arrangeGrob(grobs = index_plots, nrow = 3, ncol = 3,
                        layout_matrix = layout_grid)
 
-ggsave("figures/index_coef_plot.png", nb_grid, height = 10, width = 7)
+ggsave("figures/index_coef_plot07-04.png", nb_grid, height = 10, width = 7)
 
 coef_names <- c("Intercept", "Child Soldier\n(Binary)", "Illicit Resources", "Duration",
                 "Battle Related\nDeaths", "Population\nLiving in Slums",
@@ -152,7 +155,7 @@ layout_grid <- rbind(c(2, 3, 8),
 nb_grid <- arrangeGrob(grobs = dum_inter_plots, nrow = 3, ncol = 3,
                        layout_matrix = layout_grid)
 
-ggsave("figures/dum_inter_coef_plot.png", nb_grid, height = 10, width = 7)
+ggsave("figures/dum_inter_coef_plot07-04.png", nb_grid, height = 10, width = 7)
 
 coef_names <- c("Intercept", "Child Soldier\n(Index)", "Illicit Resources",
                 "Duration", "Battle Related\nDeaths", "Population\nLiving in Slums",
@@ -181,7 +184,79 @@ layout_grid <- rbind(c(2, 3, 8),
 nb_grid <- arrangeGrob(grobs = index_inter_plots, nrow = 3, ncol = 3,
                        layout_matrix = layout_grid)
 
-ggsave("figures/index_inter_coef_plot.png", nb_grid, height = 10, width = 7)
+ggsave("figures/index_inter_coef_plot07-04.png", nb_grid, height = 10, width = 7)
 
-# Tables ---------------------------------------------------------------------
+# Single graphs ---------------------------------------------------------------
+# Dataset 1
+dum1 <- stan_glm.nb(hom_count ~ Csdum + resources + duration +
+                                   battle_deaths + pop_slums + education,
+              data = subset(a.out$imputations[[1]], postcon == 1), cores = 4)
 
+
+ind1 <- stan_glm.nb(hom_count ~ Csindex + resources +
+                                     duration + battle_deaths + pop_slums +
+                                     education,
+              data = subset(a.out$imputations[[1]], postcon == 1), cores = 4)
+
+
+dum2 <- stan_glm.nb(hom_count ~ Csdum + resources + Csdum * resources + 
+                       duration + battle_deaths + pop_slums + education,
+              data = subset(a.out$imputations[[1]], postcon == 1), cores = 4)
+
+
+ind2 <- stan_glm.nb(hom_count ~ Csindex + resources + Csindex * resources +
+                                     duration + battle_deaths + pop_slums +
+                                     education,
+              data = subset(a.out$imputations[[1]], postcon == 1), cores = 4)
+
+coef_names <- c("Child Soldier\n(Binary)", "Illicit Resources",
+                "Duration", "Battle Related\nDeaths",
+                "Population\nLiving in Slums",
+                "Education")
+dum_plot1 <- mcmc_areas(dum1, prob = 0.5, prob_outer = 0.95,
+    point_est = "median", regex_pars = c("Cs", "resources",
+    "duration", "battle_deaths", "pop_slums",
+    "education"), area_method = "equal height") +
+    scale_y_discrete(labels = c(Csdum = "Child Soldier\n(Binary)",
+        resources = "Illicit Resources", duration = "Duration",
+        battle_deaths = "Battle Related\nDeaths",
+        pop_slums = "Population\nLiving in Slums",
+        education = "Education"))
+ggsave("figures/dum_plot1_07-04.png", dum_plot1, height = 10, width = 7)
+
+ind_plot1 <- mcmc_areas(ind1, prob = 0.5, prob_outer = 0.95,
+    point_est = "median", regex_pars = c("Cs", "resources",
+    "duration", "battle_deaths", "pop_slums",
+    "education"), area_method = "equal height") + 
+    scale_y_discrete(breaks = c("duration", "battle_deaths", "pop_slums",
+        "education", "resources", "Csindex"),
+        labels = c(duration = "Duration",
+        battle_deaths = "Battle Related\nDeaths",
+        pop_slums = "Population\nLiving in Slums",
+        education = "Education", resources = "Illicit Resources",
+        Csindex = "Child Soldier\n(Index)"))
+ggsave("figures/ind_plot1_07-04.png", ind_plot1, height = 10, width = 7)
+
+dum_plot2 <- mcmc_areas(dum2, prob = 0.5, prob_outer = 0.95,
+    point_est = "median", regex_pars = c("Cs", "resources",
+    "duration", "battle_deaths", "pop_slums",
+    "education"), area_method = "equal height") + 
+    scale_y_discrete(labels = c(Csdum = "Child Soldier\n(Binary)",
+        resources = "Illicit Resources", 
+        "Csdum:resources" = "Child Soldier\n(Binary) x\nIllicit Resources",
+        duration = "Duration",
+        battle_deaths = "Battle Related\nDeaths",
+        pop_slums = "Population\nLiving in Slums",
+        education = "Education"))
+ggsave("figures/dum_plot2_07-04.png", dum_plot2, height = 10, width = 7)
+
+ind_plot2 <- mcmc_areas(ind2, prob = 0.5, prob_outer = 0.95,
+    point_est = "median", regex_pars = c("Cs", "resources",
+    "duration", "battle_deaths", "pop_slums",
+    "education"), area_method = "equal height") + 
+    scale_y_discrete(labels = c(Csdum = "Child Soldier\n(Binary)",
+        resources = "Illicit Resources", duration = "Duration",
+        battle_deaths = "Battle Related\nDeaths",
+        pop_slums = "Population\nLiving in Slums",
+        education = "Education"))
+ggsave("figures/ind_plot2_07-04.png", ind_plot2, height = 10, width = 7)
